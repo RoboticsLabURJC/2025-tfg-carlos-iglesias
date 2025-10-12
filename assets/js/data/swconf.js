631ba3cdebf3fbc15b80c988533705227bf11e65
ciglesias1995 <ciglesias1995@users.noreply.github.com>
2025-10-12 12:04:40 +0000

    deploy: c76074e187d312640666be4e38d1a1062a936e7b

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1760270593',resources: [
+    cacheName: 'chirpy-1760270679',resources: [
       '/2025-tfg-carlos-iglesias/assets/css/jekyll-theme-chirpy.css',
       '/2025-tfg-carlos-iglesias/',
       
