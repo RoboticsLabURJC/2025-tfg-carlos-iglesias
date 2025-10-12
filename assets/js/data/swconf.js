9a31d6f7c5bcddaf61e4fd78a2b37a3c44cedaf2
ciglesias1995 <ciglesias1995@users.noreply.github.com>
2025-10-12 11:59:44 +0000

    deploy: f7329c1b33a64e1a018a25e6de50ddcde63e62e4

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1760270384',resources: [
+      '/2025-tfg-carlos-iglesias/assets/css/jekyll-theme-chirpy.css',
+      '/2025-tfg-carlos-iglesias/',
+      
+        '/2025-tfg-carlos-iglesias/categories/',
+      
+        '/2025-tfg-carlos-iglesias/tags/',
+      
+        '/2025-tfg-carlos-iglesias/archives/',
+      
+        '/2025-tfg-carlos-iglesias/about/',
+      
+
+      
+      
+    ],allowHosts: [
+      
+          'fonts.googleapis.com',
+          'fonts.gstatic.com',
+          'fonts.googleapis.com',
+          'cdn.jsdelivr.net',
+          'cdnjs.cloudflare.com'
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
